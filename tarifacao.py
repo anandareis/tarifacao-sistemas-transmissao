c314f0a837a28a136e60400c288eda4ec027753c
Ananda Reis <ananda.eloa@hotmail.com>
2020-08-22 12:21:20 -0300

    Adicionar os arquivos de teste do 118 barras

--- tarifacao.py
+++ tarifacao.py
@@ -11,7 +11,7 @@ parser = argparse.ArgumentParser(description='Calcula as tarifas nodais e zonais
 opcoes = parser.add_subparsers(required=True)
 
 teste = opcoes.add_parser('teste', help='Utilizar um dos sistemas-teste disponíveis no programa')
-teste.add_argument('sistema_teste', type=str, choices=['5_barras', '24_barras'], help='Sistema-teste a ser utilizado')
+teste.add_argument('sistema_teste', type=str, choices=['5_barras', '24_barras','118_barras'], help='Sistema-teste a ser utilizado')
 teste.add_argument('-r', '--barra-referencia', metavar='B_REF', type=int, help='Número da barra de referência para o cálculo', default=1)
 teste.add_argument('-p', '--proporcao-geracao', metavar='PROP', type=float, help='Proporção da tarifa correspondente à geração, em relação à carga', default=50)
 
